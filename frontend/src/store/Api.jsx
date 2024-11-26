5e4ed5f0f12faf835297b03b4e1183ce7f05fb81
AleemAhmad3 <ahmadaleem592@gmail.com>
2024-11-26 11:48:12 +0500

    Update Api.jsx

--- frontend/src/store/Api.jsx
+++ frontend/src/store/Api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
+
 const API_URL = 'https://food-delivery-nnwo.onrender.com/api/cart';
 
 export const addToCart = async (token, itemid, quantity = 1) => {
@@ -10,17 +10,9 @@ export const addToCart = async (token, itemid, quantity = 1) => {
                 Authorization: `Bearer ${token}`
             }
         });
-
-        // Show success toast message
-        toast.success('Item added to cart!', { position: "bottom-right" });
-
         return response.data;
     } catch (error) {
         console.error("Error adding to cart:", error);
-
-        // Show error toast message
-        toast.error('Failed to add item to cart!', { position: "bottom-right" });
-
         return { success: false, message: "Failed to add to cart." }; 
     }
 };
@@ -32,21 +24,9 @@ export const removeFromCart = async (token, itemid) => {
                 Authorization: `Bearer ${token}`
             }
         });
-
-        // Show success toast message
-        toast.success('Item removed from cart!', { position: "bottom-right" });
-
         return response.data; 
     } catch (error) {
         console.error("Error removing from cart:", error);
-
-        // Show error toast message
-        toast.error('Failed to remove item from cart!', { position: "bottom-right" });
-
-        if (error.response) {
-            return { success: false, message: error.response.data.message || "Failed to remove from cart." }; 
-        }
-        
         return { success: false, message: "Failed to remove from cart due to a network error." }; 
     }
 };
@@ -62,10 +42,6 @@ export const getCart = async (token) => {
         return response.data;
     } catch (error) {
         console.error("Error fetching cart:", error);
-
-        // Show error toast message
-        toast.error('Failed to fetch cart items!', {position: "bottom-right" });
-
         return { success: false, message: "Failed to fetch cart items." };
     }
 };
@@ -78,18 +54,9 @@ export const updateCartQuantity = async (token, itemid, action) => {
                 Authorization: `Bearer ${token}`
             }
         });
-
-        // Show success toast message
-        const actionText = action === "increase" ? 'increased' : 'decreased';
-        toast.success(`Item quantity ${actionText}!`, { position: "bottom-right" });
-
         return response.data; 
     } catch (error) {
         console.error("Error updating cart quantity:", error);
-
-        // Show error toast message
-        toast.error('Failed to update cart quantity!', { position: "bottom-right" });
-
         throw error;
     }
 };
